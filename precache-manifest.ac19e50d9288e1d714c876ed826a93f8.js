a9686dd32614c89222110ea5e0e3cc90405fc00b
Chandan <dchandankrsingh@gmail.com>
2022-02-15 16:51:26 +0530

    Updates

---
+++ precache-manifest.ac19e50d9288e1d714c876ed826a93f8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "aa002c03172cb7555aeb4105dd9bb039",
+    "url": "/quiz-app/index.html"
+  },
+  {
+    "revision": "c8e992b37e8d1c9520c7",
+    "url": "/quiz-app/static/css/main.51c93a64.chunk.css"
+  },
+  {
+    "revision": "cda735c6d0a5a231316f",
+    "url": "/quiz-app/static/js/2.2b501dd3.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/quiz-app/static/js/2.2b501dd3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c8e992b37e8d1c9520c7",
+    "url": "/quiz-app/static/js/main.5a14ec58.chunk.js"
+  },
+  {
+    "revision": "2b8e60fa09f000520361",
+    "url": "/quiz-app/static/js/runtime-main.d5202142.js"
+  }
+]);
